358e3b72a8cfbd152a8a0f3263376e5e0b14b892
Miftahadin Mohamed <miftahadinmohamed99@gmail.com>
2026-07-05 12:15:40 +0300

    update script.js:select DOM elements

---
+++ script.js
@@ -0,0 +1,11 @@
+// Select HTML elements
+
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const clearBtn = document.getElementById("clearBtn");
+const taskList = document.getElementById("taskList");
+const errorMsg = document.getElementById("errorMsg");
+const remainingCount = document.getElementById("remainingCount");
+const counter = document.getElementById("counter");
+const allDoneMsg = document.getElementById("allDoneMsg");
+const colorCircles = document.querySelectorAll(".color-circle");
